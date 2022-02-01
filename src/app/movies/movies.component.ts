64a08ccc53d21242d28c103b73da21a37c30f4d5
sefaapinar <sefanur.pinar@gmail.com>
2022-02-01 12:05:22 +0300

    Template

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -1,9 +1,21 @@
 import { Component } from "@angular/core";    //Yeni Oluşturuldu.
+import { Movie } from "../movie";
 
 @Component({
   selector: 'movies',  //<movies></movies>
   templateUrl: 'movies.component.html'
+
 })
 export class MoviesComponent {
+      title = 'Movie List'
 
+      movie : Movie = {
+        id:1,
+        name: 'Movie Name'
+
+      }
+
+      getTitle(){
+        return this.title;
+      }
 }
